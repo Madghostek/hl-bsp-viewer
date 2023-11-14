9c070086b70f9a9da18fe55a5ebdf3d4f580b49d
Madghostek <madghostek@gmail.com>
2023-11-15 00:34:08 +0100

    add basic tests for triangles separation

--- test/test_SeparateVertices.py
+++ test/test_SeparateVertices.py
@@ -1,7 +1,16 @@
 from triangle_math import SeparateVertices
+import pytest
+import numpy as np
 
-def test_SeparateVertices():
-	vertices = [(0,1,2),(1,1,1),(1,2,1)]
-	triangles = [(0,1,2)]
-	result = SeparateVertices(vertices,triangles)
-	print(result)
+def test_SeparateVertices_nothingtodo():
+	vertices = np.array([(0,1,2),(1,1,1),(1,2,1)])
+	triangles = np.array([(0,1,2)])
+	new_triangles,new_vertices = SeparateVertices(triangles, vertices)
+	assert all(new_vertices==vertices.flatten())
+
+def test_SeparateVertices_separateone():
+	vertices = np.array([(0,0,0),(1,1,1),(2,2,2),(3,3,3),(4,4,4)])
+	triangles = np.array([(0,1,2),(0,3,4)])
+	target = np.array([(0,0,0),(1,1,1),(2,2,2),(0,0,0),(3,3,3),(4,4,4)])
+	new_triangles,new_vertices = SeparateVertices(triangles, vertices)
+	assert all(new_vertices==target.flatten())
